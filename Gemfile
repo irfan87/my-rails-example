1345816211558445b2dd9b3dcd657a8942ec89d2
irfan87 <nerve2009@yahoo.com>
2018-04-07 07:53:05 +0800

    capybara setup

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 
 group :development, :test do
   gem 'better_errors'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'capybara/rspec'
+require 'capybara/webkit/matchers'
 require 'simple_bdd'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -29,6 +30,8 @@ require 'simple_bdd'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include SimpleBdd, type: :feature
+  Capybara.javascript_driver = :webkit
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -56,8 +59,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include SimpleBdd, type: :feature
+  
   # config.include Devise::TestHelpers, type: :controller
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
